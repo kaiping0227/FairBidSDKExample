5912a75c305c282521144f1f75d31bee2d1907c0
kaiping.tseng <francis830227@gmail.com>
2022-12-07 14:10:53 +0100

    able to show banner ad & rewarded ad

--- FairBidSDKExample/ViewController.swift
+++ FairBidSDKExample/ViewController.swift
@@ -9,12 +9,67 @@ import UIKit
 import FairBidSDK
 
 class ViewController: UIViewController {
-
+    
+    private let rewardedPlacementID = "197406"
+    private let bannerPlacementID = "197407"
+    
+    @IBOutlet weak var rewardButton: UIButton!
+    @IBOutlet weak var requestButton: UIButton!
+    @IBOutlet weak var bannerView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        FYBBanner.delegate = self
+        FYBRewarded.delegate = self
+    }
+    
+    @IBAction func bannerButtonDidTap(_ sender: Any) {
+        fetchingInProgress()
+        let bannerOptions = FYBBannerOptions(placementId: bannerPlacementID, size: .smart)
+        FYBBanner.show(in: bannerView, options: bannerOptions)
+    }
+    
+    @IBAction func rewardButtonDidTap(_ sender: Any) {
+        FYBRewarded.show(rewardedPlacementID)
     }
 
-
+    @IBAction func requestButtonDidTap(_ sender: Any) {
+        fetchingInProgress()
+        FYBRewarded.request(rewardedPlacementID)
+    }
+    
+    func fetchingInProgress() {
+        requestButton.setTitleColor(.clear, for: .normal)
+        requestButton.isEnabled = false
+    }
+    
+    func adIsAvailable() {
+        rewardButton.isEnabled = true
+        requestButton.setTitleColor(.white, for: .normal)
+        requestButton.isEnabled = false
+    }
+    
+    func adDismissed() {
+        requestButton.setTitleColor(.white, for: .normal)
+        requestButton.isEnabled = true
+        rewardButton.isEnabled = false
+    }
 }
 
+extension ViewController: FYBRewardedDelegate {
+    
+    func rewardedIsAvailable(_ placementId: String) {
+        adIsAvailable()
+    }
+    
+    func rewardedDidDismiss(_ placementId: String) {
+        adDismissed()
+    }
+    
+}
+
+extension ViewController: FYBBannerDelegate {
+    func bannerDidLoad(_ banner: FYBBannerAdView) {
+        adIsAvailable()
+    }
+}
